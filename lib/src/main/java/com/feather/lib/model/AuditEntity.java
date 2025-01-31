79ceb3021c4ee7324ccc2f891b22c1b47e9ce302
soner3 <sonerastan@icloud.com>
2025-01-31 21:17:23 +0100

    Configured Auth Server for public client with PKCE

---
+++ lib/src/main/java/com/feather/lib/model/AuditEntity.java
@@ -0,0 +1,27 @@
+package com.feather.lib.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class AuditEntity {
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
