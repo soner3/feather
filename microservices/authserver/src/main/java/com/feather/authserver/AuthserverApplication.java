7e17cc433d62af471fe942fad172b26d582628da
soner3 <sonerastan@icloud.com>
2025-02-07 22:14:40 +0100

    Created Profile service as ressource server and client

--- microservices/authserver/src/main/java/com/feather/authserver/AuthserverApplication.java
+++ microservices/authserver/src/main/java/com/feather/authserver/AuthserverApplication.java
@@ -3,11 +3,7 @@ package com.feather.authserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.feather.authserver.config.user.UserDetailsImpl;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,13 +15,6 @@ public class AuthserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthserverApplication.class, args);
-	}
-
-	@GetMapping
-	protected String hello(Authentication authentication) {
-		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-		log.info(userDetails.getUserId());
-		return "Hello";
 	}
 
 }
